73200ed60f3c7e935298d33a4a031ea0b3c59739
SalomeAc <salome.acosta@correounivalle.edu.co>
2024-10-16 23:49:16 -0500

    vista general de todos los productos

--- tienda/tienda/settings.py
+++ tienda/tienda/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'gestion/static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
