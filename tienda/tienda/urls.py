d16983c450dcf997617af9a67b402cc9ce7d497c
Emily <emily.nunez@correounivalle.edu.co>
2024-11-08 11:00:00 -0500

    urls.py

--- tienda/tienda/urls.py
+++ tienda/tienda/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import urls
 from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('gestion.urls')),
+    path('autenticacion/', include('autenticacion.urls')),
+    path('carro/', include('carro.urls')),
+    path('pedidos/', include('pedidos.urls')),
+    path('products/', include('products.urls'))
     
 ]
