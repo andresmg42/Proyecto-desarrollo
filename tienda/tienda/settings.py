41e23e07b87ffa760ae053e8821e1cbb7c35f402
Emily <emily.nunez@correounivalle.edu.co>
2024-11-08 12:29:12 -0500

    setting.py
    Añadir las apps

--- tienda/tienda/settings.py
+++ tienda/tienda/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gestion',
+    'autenticacion',
+    'carro',
+    'pedidos',
+    'products',
 ]
 
 MIDDLEWARE = [
