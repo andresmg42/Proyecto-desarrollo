9c9629f424c76a7471c37175d6eddb7ececa9754
andres <andresdavid.ortega@gmail.com>
2024-12-14 02:53:08 -0500

    ya funcionaron las imagenes en render 2

--- tienda/settings.py
+++ tienda/settings.py
@@ -172,7 +172,10 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # corse autorization
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://classsmart-mu.vercel.app"
+    ]
 
 REST_FRAMEWORK = {
     ...: ...,
