c2bba0c59037801e4f155e4159560210555dcc4b
andres <andresdavid.ortega@gmail.com>
2025-02-07 11:33:15 -0500

    restablecimiento de DATABASE_URL

--- tienda/settings.py
+++ tienda/settings.py
@@ -102,10 +102,9 @@ WSGI_APPLICATION = "tienda.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")  # Configura esta variable en Render
+    )
 }
 
 
@@ -224,3 +223,4 @@ cloudinary.config(
 
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
