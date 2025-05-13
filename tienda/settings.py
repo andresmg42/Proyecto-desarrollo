7689246a69d542524123d273fb0a4b69595e623d
andresdavid.ortega@gmail.com <andresdavid.ortega@gmail.com>
2025-05-13 15:29:26 -0500

    build y workflow django agregados

--- tienda/settings.py
+++ tienda/settings.py
@@ -101,10 +101,17 @@ WSGI_APPLICATION = "tienda.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get("DATABASE_URL")  # Configura esta variable en Render
+#     )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")  # Configura esta variable en Render
-    )
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 }
 
 
